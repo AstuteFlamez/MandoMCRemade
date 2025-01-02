f80d5495b25101fa592e355bf35fdb3d32a9918e
AstuteFlamez <perosinal@gmail.com>
2025-01-02 16:19:37 -0600

    v0.0.28

--- src/main/java/net/mandomc/mandomcremade/tasks/StaminaTask.java
+++ src/main/java/net/mandomc/mandomcremade/tasks/StaminaTask.java
@@ -3,6 +3,7 @@ package net.mandomc.mandomcremade.tasks;
 import net.mandomc.mandomcremade.managers.StaminaManager;
 import net.mandomc.mandomcremade.objects.CustomScoreboard;
 import net.mandomc.mandomcremade.objects.Stamina;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -26,6 +27,8 @@ public class StaminaTask extends BukkitRunnable {
                 if (!player.isSprinting() && stamina.getStaminaAmount() < 100 && !stamina.isEffectOnCooldown()) {
                     stamina.addStamina(10);
                 }
+                Bukkit.broadcastMessage("" + stamina.getStaminaAmount());
+                player.setExp((float) stamina.getStaminaAmount() / 2500);
                 customScoreboard.updateScore(player, stamina.getStaminaAmount());
             }
         }
